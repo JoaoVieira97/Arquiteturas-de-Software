1de830d3ecac121bf6dad718d80c7be4f7d988f2
SimaoBarbosa <SimaoBarbosa@users.noreply.github.com>
2018-12-26 16:36:57 +0000

    small fix Main

--- BetESS_Refactoring/src/betess_refactoring/Main.java
+++ BetESS_Refactoring/src/betess_refactoring/Main.java
@@ -52,98 +52,24 @@ public class Main {
     }
     
     public static BetESSModel addApostadores(BetESSModel model) {
-        String[] dados = new String[3];
-        
-        dados[0] = "antonio@hotmail.com";
-        dados[1] = "12345";
-        dados[2] = "António Silva";
-        model.addApostador(dados, 15.60);
-        
-        dados[0] = "mafalda@hotmail.com";
-        dados[1] = "11111";
-        dados[2] = "Mafalda Castro";
-        model.addApostador(dados, 5.90);
-        
-        dados[0] = "carlos@hotmail.com";
-        dados[1] =  "22222";
-        dados[2] = "Carlos Sampaio";
-        model.addApostador(dados, 3.00);
-        
-        dados[0] = "alberto@hotmail.com";
-        dados[1] =  "33333";
-        dados[2] = "Alberto Campos";
-        model.addApostador(dados, 42.30);
-        
+        model.addApostador(new String[]{"antonio@hotmail.com","12345","António Silva"}, 15.60);
+        model.addApostador(new String[]{"mafalda@hotmail.com","11111","Mafalda Castro"}, 5.90);
+        model.addApostador(new String[]{"carlos@hotmail.com","22222","Carlos Sampaio"}, 3.00);
+        model.addApostador(new String[]{"alberto@hotmail.com","33333","Alberto Campos"}, 42.30);
         return model;
         
 
     }
     public static BetESSModel addEventos(BetESSModel model) {
-        String equipas[] = new String[2];
-        double odds[] = new double[3];
-        
-        equipas[0] = "CD Leganés";
-        equipas[1] = "Atlético Madrid";
-        odds[0]= 5.25;
-        odds[1]= 2.85;
-        odds[2]= 1.57;
-        model.addEvento(equipas,odds, true);
-        
-        equipas[0] ="Real Madrid CF";
-        equipas[1] = "Real Valladolid";
-        odds[0]=  1.19;
-        odds[1]= 6.25;
-        odds[2]= 11.00 ;
-        model.addEvento(equipas,odds, true);
-        
-        equipas[0] = "Valência CF";
-        equipas[1] = "Girona CF";
-        odds[0]=1.47 ;
-        odds[1]=  3.90;
-        odds[2]= 6.5 ;
-        model.addEvento(equipas,odds, true);
-        
-        equipas[0] = "Rayo Vallecano";
-        equipas[1] = "FC Barcelona";
-        odds[0]= 9.00;
-        odds[1]= 5.75;
-        odds[2]= 1.25;
-        model.addEvento(equipas,odds, true);
-       
-        equipas[0] = "SD Eibar";
-        equipas[1] = "CD Alavés";
-        odds[0]= 2.05;
-        odds[1]= 3.10;
-        odds[2]= 3.60;
-        model.addEvento(equipas,odds, true);
-         
-        equipas[0] = "CF Villareal";
-        equipas[1] = "UD Levante";
-        odds[0]= 1.57;
-        odds[1]= 4.00;
-        odds[2]= 5.00;
-        model.addEvento(equipas,odds, true);
-        
-        equipas[0] = "Real Sociedad";
-        equipas[1] = "Sevilha FC";
-        odds[0]= 2.65;
-        odds[1]= 3.30;
-        odds[2]= 2.40;
-        model.addEvento(equipas,odds, true);
-        
-        equipas[0] ="SD Huesca";
-        equipas[1] = "Getafe CF";
-        odds[0]= 3.50;
-        odds[1]= 3.00;
-        odds[2]= 2.15;
-        model.addEvento(equipas,odds, true);
-        
-        equipas[0] = "Bétis Sevilha";
-        equipas[1] = "Celta de Vigo";
-        odds[0]= 1.77;
-        odds[1]= 3.60;
-        odds[2]= 4.00;
-        model.addEvento(equipas,odds, true);    
+        model.addEvento(new String[]{"CD Leganés","Atlético Madrid"},new double[]{5.25,2.85,1.57}, true);
+        model.addEvento(new String[]{"Real Madrid CF","Real Valladolid"},new double[]{1.19,6.25,11.00}, true);
+        model.addEvento(new String[]{"Valência CF","Girona CF"},new double[]{1.47,3.90,6.5}, true);
+        model.addEvento(new String[]{"Rayo Vallecano","FC Barcelona"},new double[]{9.00,5.75,1.25}, true);
+        model.addEvento(new String[]{"SD Eibar","CD Alavés"},new double[]{2.05,3.10,3.60}, true);
+        model.addEvento(new String[]{"CF Villareal","UD Levante"},new double[]{1.57,4.00,5.00}, true);
+        model.addEvento(new String[]{"Real Sociedad","Sevilha FC"},new double[]{2.65,3.30,2.40}, true);
+        model.addEvento(new String[]{"SD Huesca","Getafe CF"},new double[]{3.50,3.00,2.15}, true);
+        model.addEvento(new String[]{"Bétis Sevilha","Celta de Vigo"},new double[]{1.77,3.60,4.00}, true);
         
         return model;
     }
